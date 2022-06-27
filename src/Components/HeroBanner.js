de74fd7331e680d52ec2a1dc0a38291d0cc01e97
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-06-27 12:28:48 +0100

    Update HeroBanner

--- src/Components/HeroBanner.js
+++ src/Components/HeroBanner.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
 
 const HeroBanner = () => {
-  return <div> HeroBanner </div>;
+  return (
+    <Box>
+      <Typography>Fitness Club</Typography>
+      <Typography>Get Fit, <br/> Don't Quit</Typography>
+      <Stack></Stack>
+      <Typography>
+        “All progress takes place outside the comfort zone.”
+      </Typography>
+      <Typography>
+        Exercise
+      </Typography>
+    </Box>
+  );
 };
 
 export default HeroBanner;
