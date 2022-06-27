8fad7feb3cd5b7e376abbe7f14212500e3687ac7
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-06-27 12:10:22 +0100

    Update Home
    Hero Banner
    Bmi
    Search Exercise
    Exercise

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { Box } from "@mui/material";
+import HeroBanner from "../Components/HeroBanner";
+import SearchExercise from "../Components/SearchExercise";
+import Bmi from "../Components/Bmi";
+import Exercise from "../Components/Exercise";
+
 const Home = () => {
   return (
-<div> Home </div>
-
-  ) 
-}; 
+    <Box>
+        <HeroBanner />
+        <Bmi />
+        <SearchExercise />
+        <Exercise />
+    </Box>
+  )
+};
 
 export default Home;
