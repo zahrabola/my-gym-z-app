059c42f3948848ff4de87c18e66a6cfaf08eb818
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-06-27 11:32:13 +0100

    Update Navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,13 +6,46 @@ import { Stack } from "@mui/material";
 import Logo from "../images/Logo.png";
 const Navbar = () => {
     return (
-       
-        <Stack>
-            <Link to="/">
-            <img src={Logo} alt="logo" />
-            </Link>
+      <Stack
+        direction="row"
+        justifyContent="space-around"
+        sx={{
+          gap: { sm: "123px", xs: "40px" },
+          mt: { sm: "32px", xs: "20px" },
+          justifyContent: "none",
+        }}
+        px="20px"
+      >
+        <Link to="/">
+          <img
+            src={Logo}
+            alt="logo"
+            style={{
+              width: "50px",
+              height: "50px",
+              margin: "0, 20px",
+            }}
+          />
+        </Link>
+        <Stack
+          direction="row"
+          gap="50px"
+          fontFamily="Alegreya"
+          fontSize="24px"
+          alignItems="flex-end"
+        >
+          <Link to="/" style={{ textDecoration: "none", color: "#e30000", borderBottom: '3px solid #FF2625'}}>
+            Home
+          </Link>
+          <a
+            href="#exercises"
+            style={{ textDecoration: "none", color: "#e30000" }}
+          >
+            Exercise
+          </a>
         </Stack>
-    )
+      </Stack>
+    );
 }
 
 
