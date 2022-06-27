5514b55d90a6b48d72fb0cf879d877a502ccd3fb
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-06-27 10:50:53 +0100

    Update app
    Footer
    Navbar
    Home

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import { Route, Routes } from "react-router-dom";
 import { Box } from "@mui/material";
 import './App.css';
+import ExerciseDetail from "./Pages/ExerciseDetail";
+import Home from "./Pages/Home";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer"
 
 function App() {
   return (
     <Box width="400px">
-      Navbar
+      <Navbar />
       <Routes>
-        <Route path="/" eleement={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/exercise/:id" eleement={<ExerciseDetail />} />
       </Routes>
+      <Footer />
     </Box>
   );
 }
