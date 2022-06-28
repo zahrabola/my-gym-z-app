75821d1ba5468ecd47827cfcfb1768adc6db5f6e
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-06-28 14:44:10 +0100

    Update Body Part

--- src/Components/BodyParts.js
+++ src/Components/BodyParts.js
@@ -10,11 +10,41 @@ const BodyParts = ({ item, setBodyPart, bodyPart }) => {
       alignItems="center"
       justifyContent="center"
       className="bodypartcard"
-      sx={bodyPart === item ? {borderTop:"4ps solid #FF2625", background: "#fff",borderBottomLeftRadius: '20px', width: '270px', height: '282px', cursor: 'pointer', gap: '40px' } : { background: '#fff', borderBottomLeftRadius: '20px', width: '270px', height: '282px', cursor: 'pointer', gap: '40px' }} 
-      
+      sx={
+        bodyPart === item
+          ? {
+              borderTop: "4ps solid #FF2625",
+              background: "#fff",
+              borderBottomLeftRadius: "20px",
+              width: "270px",
+              height: "282px",
+              cursor: "pointer",
+              gap: "40px",
+            }
+          : {
+              background: "#fff",
+              borderBottomLeftRadius: "20px",
+              width: "270px",
+              height: "282px",
+              cursor: "pointer",
+              gap: "40px",
+            }
+      }
+    >
+      <img
+        src={Icon}
+        alt="dumbell"
+        style={{ width: "140px", height: "140px" }}
+      />
+      <Typography
+        fontSize="24px"
+        fontWeight="bold"
+        fontFamily="Alegreya"
+        color="#FF2625"
+        textTransform="capitalize"
       >
-      <img src={Icon} alt="dumbell" style={{ width: "140px", height: "140px" }} />
-      <Typography></Typography>
+        {item}
+      </Typography>
     </Stack>
   );
 };
