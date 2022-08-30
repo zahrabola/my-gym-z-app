09f39ced93e623458f22284bac4f77f44ba710b4
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-08-30 12:45:17 +0100

    Update Footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { Box, Typography } from "@mui/material";
 
 const Footer = () => {
-  return  (
-  
- <div> Footer </div>
-  )
+  return (
+    <Box mt="80px" bgcolor="#FFF3F4">
+ 
+      <Typography
+        variant="h5"
+        sx={{ fontSize: { lg: "28px", xs: "20px" } }}
+        mt="41px"
+        textAlign="center"
+        pb="40px"
+      >
+       Zahra Olanrewaju
+      </Typography>
+    </Box>
+  );
 };
 
 export default Footer;
